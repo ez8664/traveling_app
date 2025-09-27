d59eedc73327e86f4c516333a7a80b12c8cdf0b7
ez8664 <ez8664@g.rit.edu>
2025-09-26 22:23:56 -0400

    Header of the Web done

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,6 +1,17 @@
+import { Header } from "components"
+
 const dashboard = () => {
+  const user = { name: "Adrian" }
+
   return (
-    <div>Dashboard Page</div>
+    <main className="dashboard wrapper">
+      <Header 
+        title={`Welcome ${user?.name ?? 'Guest'} 👋`}
+        description="Track activity, trends and popular destinations in real time"
+      />
+
+      Dashboard Page Contents
+    </main>
   )
 }
 
